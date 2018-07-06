9508965b5580ac4b2041043518c0774efceb0d4b
Dylan Blanchard <dylanblanchard@Dylans-MacBook-Pro.local>
2018-07-06 09:52:26 -0500

    added success stories and linked on every header

--- js/functions.js
+++ js/functions.js
@@ -3133,6 +3133,36 @@ var SEMICOLON = SEMICOLON || {};
 
 				return false;
 			});
+			$("div[data-scrollto]").click(function(){
+				var element = $(this),
+					divScrollToAnchor = element.attr('data-scrollto'),
+					divScrollSpeed = element.attr('data-speed'),
+					divScrollOffset = element.attr('data-offset'),
+					divScrollEasing = element.attr('data-easing'),
+					divScrollHighlight = element.attr('data-highlight');
+
+				if( element.parents('#primary-menu').hasClass('on-click') ) { return true; }
+
+				if( !divScrollSpeed ) { divScrollSpeed = 750; }
+				if( !divScrollOffset ) { divScrollOffset = SEMICOLON.initialize.topScrollOffset(); }
+				if( !divScrollEasing ) { divScrollEasing = 'easeOutQuad'; }
+
+				$('html,body').stop(true).animate({
+					'scrollTop': $( divScrollToAnchor ).offset().top - Number(divScrollOffset)
+				}, Number(divScrollSpeed), divScrollEasing, function(){
+					if( divScrollHighlight ) {
+						if( $(divScrollToAnchor).find('.highlight-me').length > 0 ) {
+							$(divScrollToAnchor).find('.highlight-me').animate({'backgroundColor': divScrollHighlight}, 300);
+							var t = setTimeout( function(){ $(divScrollToAnchor).find('.highlight-me').animate({'backgroundColor': 'transparent'}, 300); }, 500 );
+						} else {
+							$(divScrollToAnchor).animate({'backgroundColor': divScrollHighlight}, 300);
+							var t = setTimeout( function(){ $(divScrollToAnchor).animate({'backgroundColor': 'transparent'}, 300); }, 500 );
+						}
+					}
+				});
+
+				return false;
+			});
 		},
 
 		contactForm: function(){
